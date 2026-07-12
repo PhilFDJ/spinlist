3533db885b3c08d77e9f16ea2641b7d5ee9e6a5e
PhilFDJ <phil@phil-freeman.co.uk>
2026-07-12 09:53:10 +0100

    Delete spinlist-backend 23 directory

--- spinlist-backend 23/app.js
+++
@@ -1,4 +0,0 @@
-/* cPanel / Phusion Passenger entry point.
-   Passenger looks for "app.js" by default. We simply hand off to
-   server.js, which detects Passenger and exports the app correctly. */
-module.exports = require('./server.js');
